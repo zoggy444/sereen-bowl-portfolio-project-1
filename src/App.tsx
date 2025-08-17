ef64682df869acbb195fbb31016ead489dbca425
Nilrem <43614933+zoggy444@users.noreply.github.com>
2025-08-17 03:15:34 +0200

    ✨ F01 completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
+import { useState } from 'react'
 
 function App() {
+  const [userName, setUserName] = useState('');
 
   return (
     <>
+      <h1>Welcome to our place !</h1>
+      <h2>Log in</h2>
+      <form>
+        <input name='name' type='text' required
+            placeholder='Enter your name...'
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+            onSubmit={() => setUserName('')}>
+        </input>
+        <button type='submit' onClick={(e) => {
+            e.preventDefault();
+            if (userName) alert(`Hello ${userName}`)
+            setUserName('');
+          }}>
+          Get to your space
+        </button>
+      </form>      
     </>
   )
 }
