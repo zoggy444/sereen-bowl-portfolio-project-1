7caab9f996567893463aec78d533bf76c9fe8c62
Nilrem <garrok.dev@gmail.com>
2025-08-25 04:06:57 +0200

    💄 F03: LOGIN > Fixed overflow

--- src/components/pages/login/LoginPage.tsx
+++ src/components/pages/login/LoginPage.tsx
@@ -3,21 +3,22 @@ import LoginForm from "./LoginForm";
 import styled from "styled-components";
 
 export default function LoginPage() {
-
   return (
     <LoginPageStyled>
-      <LogoTitle/>
-      <LoginForm/>
+      <LogoTitle />
+      <LoginForm />
     </LoginPageStyled>
-  )
+  );
 }
 
 const LoginPageStyled = styled.div`
-  background: linear-gradient( rgba(0, 0, 0, 0.85), rgba(0, 150, 50, 0.25) ),url("../../../../hero-by-yoav-aziz.jpg");
-  background-repeat:no-repeat;
-  background-size:cover;
-  background-position:center;
+  background: linear-gradient(rgba(0, 0, 0, 0.85), rgba(0, 150, 50, 0.25)),
+    url("../../../../hero-by-yoav-aziz.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
   position: fixed;
+  overflow: auto;
   top: 0;
   left: 0;
   bottom: 0;
@@ -27,4 +28,4 @@ const LoginPageStyled = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-`
+`;
