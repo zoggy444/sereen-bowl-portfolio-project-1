b914c58960ae83245ca2b16f3d741751fa184148
Nilrem <garrok.dev@gmail.com>
2025-08-25 03:27:53 +0200

    💄 F03: LOGIN > Fixed input placeholder

--- src/components/reusable-ui/InputText.tsx
+++ src/components/reusable-ui/InputText.tsx
@@ -1,41 +1,43 @@
-import type { InputTextProps } from '../../types'
-import styled from 'styled-components';
-import { theme } from '../../theme/theme';
+import type { InputTextProps } from "../../types";
+import styled from "styled-components";
+import { theme } from "../../theme/theme";
 
-export default function InputText({Icon, value, onChange, ...otherProps}: InputTextProps) {
+export default function InputText({
+  Icon,
+  value,
+  onChange,
+  ...otherProps
+}: InputTextProps) {
   return (
     <InputTextStyled className="open-sans-medium">
-      <Icon className="input-icon"/>
-      <input
-          value={value}
-          onChange={onChange}
-          {...otherProps}>
-      </input>
+      <Icon className="input-icon" />
+      <input value={value} onChange={onChange} {...otherProps}></input>
     </InputTextStyled>
-  )
+  );
 }
 
 const InputTextStyled = styled.div`
-    padding: ${theme.spacing.md};
-    margin-bottom: ${theme.spacing.md};
-    border-radius: ${theme.borderRadius.round};
-    background-color: ${theme.colors.white};
+  padding: ${theme.spacing.md};
+  margin-bottom: ${theme.spacing.md};
+  border-radius: ${theme.borderRadius.round};
+  background-color: ${theme.colors.white};
 
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 
   .input-icon {
     color: ${theme.colors.greyDark};
     font-size: ${theme.fonts.P1};
-    margin-right: ${theme.spacing.sm}
+    margin-right: ${theme.spacing.sm};
   }
 
   input {
     border: none;
     font-size: ${theme.fonts.P0};
     width: 100%;
-  }
-  input:placeholder {
-    color: ${theme.colors.greyMedium};
+    &::placeholder {
+      color: ${theme.colors.greyMedium};
+      opacity: 0.8;
+    }
   }
 `;
