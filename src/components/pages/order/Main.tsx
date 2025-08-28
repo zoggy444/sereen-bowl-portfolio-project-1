f615eb342311f92064b9c9ff1df0f8adf558922f
Nilrem <garrok.dev@gmail.com>
2025-08-28 05:17:29 +0200

    💄 F05: ORDER > Added product grid layout

--- src/components/pages/order/Main.tsx
+++ src/components/pages/order/Main.tsx
@@ -2,13 +2,42 @@ import styled from "styled-components";
 import { theme } from "../../../theme/theme";
 
 export default function Main() {
-  return <MainStyled></MainStyled>;
+  const products = Array<string>(12).fill("product");
+
+  return (
+    <MainStyled>
+      <div className="product-grid">
+        {products.map((p) => (
+          <div className="product-card">{p}</div>
+        ))}
+      </div>
+    </MainStyled>
+  );
 }
 
 const MainStyled = styled.div`
-  height: 83vh;
+  padding: 50px;
+  height: calc(83vh - 100px);
+  overflow-y: scroll;
+  scrollbar-width: none;
+
   background-color: ${theme.colors.greyExtraLight};
   box-shadow: ${theme.shadows.strong};
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
+
+  .product-grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+    grid-template-rows: repeat(auto-fit, minmax(330px, 1fr));
+    gap: 60px;
+  }
+
+  .product-card {
+    grid-area: span 1 / span 1;
+    justify-self: center;
+    width: 240px;
+    height: 330px;
+    background: #e84343;
+  }
 `;
