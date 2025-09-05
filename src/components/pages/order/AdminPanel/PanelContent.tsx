48e40987a22376f26826c0bd09449330675e3d07
Nilrem <43614933+zoggy444@users.noreply.github.com>
2025-09-05 05:31:30 +0200

    💄 F08: ORDER > AdminPanel > Added form layout

--- src/components/pages/order/AdminPanel/PanelContent.tsx
+++ src/components/pages/order/AdminPanel/PanelContent.tsx
@@ -3,12 +3,26 @@ import { theme } from "../../../../theme/theme";
 import type { PanelContentProps } from "../../../../types";
 
 export default function PanelContent({ isFolded, content }: PanelContentProps) {
-  if (!isFolded) return <PanelContentStyled>{content}</PanelContentStyled>;
-  return; //<PanelFoldedStyled />;
+  if (!isFolded)
+    return (
+      <PanelContentStyled>
+        <div className="form">
+          <div className="image"></div>
+          <div className="fields">
+            <div className="field"></div>
+            <div className="field"></div>
+            <div className="field"></div>
+          </div>
+          <div className="submit"></div>
+        </div>
+      </PanelContentStyled>
+    );
+  return <PanelFoldedStyled />;
 }
 
 const PanelContentStyled = styled.div`
-  flex: 1;
+  /* flex: 1; */
+  /* height: calc(264px - ${theme.gridUnit * 5}px) */
 
   border-top: 1px solid ${theme.colors.greyLight};
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
@@ -20,6 +34,39 @@ const PanelContentStyled = styled.div`
   background-color: ${theme.colors.white};
 
   font-size: ${theme.fonts.size.P0};
+
+  /* display: flex; */
+  .form {
+    /* flex: 1; */
+    max-width: 880px;
+    border: 1px solid blue;
+
+    display: grid;
+    grid-template-columns: 25% 1fr 1fr;
+    grid-template-rows: repeat(4, ${theme.gridUnit * 5}px);
+    column-gap: ${theme.spacing.md};
+    row-gap: ${theme.spacing.xs};
+
+    .image {
+      grid-area: 1 / 1 / 4 / 2;
+      background-color: aqua;
+    }
+    .fields {
+      grid-area: 1 / 2 / 4 / 4;
+      background-color: beige;
+      display: flex;
+      flex-direction: column;
+      gap: ${theme.spacing.xs};
+      .field {
+        flex: 1;
+        background-color: coral;
+      }
+    }
+    .submit {
+      grid-area: 4 / 2 / 5 / 3;
+      background-color: yellow;
+    }
+  }
 `;
 
 const PanelFoldedStyled = styled.div`
