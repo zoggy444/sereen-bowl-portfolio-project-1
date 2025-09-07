e2058640c1f95063889627d1f82b42652a3bdd1e
Nilrem <43614933+zoggy444@users.noreply.github.com>
2025-09-07 01:55:40 +0000

    💄 F08: ORDER > MenuCard > Added delete icon

--- src/components/pages/order/Menu/MenuCard.tsx
+++ src/components/pages/order/Menu/MenuCard.tsx
@@ -3,10 +3,22 @@ import { theme } from "../../../../theme/theme";
 import Image from "../../../reusable-ui/Image";
 import ProductDetail from "./ProductDetail";
 import type { MenuCardProps } from "../../../../types";
+import { TiDelete } from "react-icons/ti";
+import IsAdminModeContext from "../../../../context/IsAdminModeContext";
+import { useContext } from "react";
 
 export default function MenuCard({ src, title, price }: MenuCardProps) {
+  const isAdminMode = useContext(IsAdminModeContext).isAdminMode;
+
   return (
     <MenuCardStyled>
+      <div className="delete-container">
+        {isAdminMode && (
+          <div className="product-delete">
+            <TiDelete />
+          </div>
+        )}
+      </div>
       <Image src={src} alt="product-image" className="product-image" />
       <ProductDetail title={title} price={price} />
     </MenuCardStyled>
@@ -26,11 +38,21 @@ const MenuCardStyled = styled.div`
 
   box-sizing: border-box;
   padding: ${theme.spacing.md};
-  padding-top: ${theme.spacing.xl};
+  padding-top: ${theme.spacing.sm};
   padding-bottom: ${theme.spacing.sm};
   border-radius: ${theme.borderRadius.extraRound};
   background: ${theme.colors.white};
   box-shadow: ${theme.shadows.card};
+
+  .delete-container {
+    height: ${theme.gridUnit * 4}px;
+    align-self: flex-end;
+    .product-delete {
+      cursor: pointer;
+      color: ${theme.colors.primary};
+      font-size: ${theme.fonts.size.P2};
+    }
+  }
 
   .product-image {
     height: 145px;
