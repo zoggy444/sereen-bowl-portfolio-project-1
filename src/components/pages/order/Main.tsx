8d53d203ebde6c62186de153bb0f6250ddba4ed7
Nilrem <43614933+zoggy444@users.noreply.github.com>
2025-09-08 00:39:59 +0000

    💄 F10: ORDER > Basket > Added layout

--- src/components/pages/order/Main.tsx
+++ src/components/pages/order/Main.tsx
@@ -7,9 +7,12 @@ import AdminPanel from "./AdminPanel/AdminPanel";
 export default function Main() {
   return (
     <MainStyled>
-      {/* <div className="basket"/> */}
-      <Menu />
-      <AdminPanel />
+      <div className="basket">
+        <div className="basket-header"></div>
+        <div className="basket-main"></div>
+        <div className="basket-footer"></div>
+      </div>
+      <Menu products={products} />
     </MainStyled>
   );
 }
@@ -23,4 +26,23 @@ const MainStyled = styled.div`
   box-shadow: ${theme.shadows.strong};
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
+
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+
+  .basket {
+    height: calc(83vh - 100px);
+    background-color: aqua;
+    display: grid;
+    grid-template-rows: 70px 1fr 70px;
+    .basket-header {
+      background-color: ${theme.colors.background_dark};
+    }
+    .basket-main {
+      /* background-color: ${theme.colors.background_white}; */
+    }
+    .basket-footer {
+      background-color: ${theme.colors.background_dark};
+    }
+  }
 `;
